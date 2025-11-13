fde0caa9fb02b111fc315c4be776e68e06e84c85
WistantKode <wistantkode@protonmail.com>
2025-11-13 04:07:52 +0100

    feat(components): add a news shadcn ui component

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,6 +1,12 @@
+import LoginForm from "@/components/login/LoginForm.tsx";
+
 export const Login = () => {
     return (
-        <div>Login</div>
+        <div className="h-dvh flex items-center justify-center p-6 md:p-10">
+            <div className={'max-w-sm md:max-w-3xl w-full'}>
+                <LoginForm/>
+            </div>
+        </div>
     )
 }
 export default Login
